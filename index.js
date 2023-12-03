f02d40da50d2869c43dd20c58b8527a5d972c7cc
hasibmuhammad <hasibmu99@gmail.com>
2023-12-03 18:52:17 +0600

    Add Brand API created and Insert into MongoDB - Done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 
@@ -10,11 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
-
-// Welcome route
-app.get("/", (req, res) => {
-  res.send(`<h1>Welcome to Foodie Backend!</h1>`);
-});
 
 // MongoDB
 const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.er9gvke.mongodb.net/?retryWrites=true&w=majority`;
@@ -41,20 +37,23 @@ const run = async () => {
     // creating db and collections
     const brandCollection = client.db("foodieDB").collection("brands");
 
-    // Brand Routes
-
-    // Get all brands
-    app.get("/brands", (req, res) => {
-      res.send({ sucess: true, msg: "All brand hits!" });
-    });
-
     // Add brand
-    app.post("/addbrand", async (req, res) => {});
-  } catch (error) {
-    console.error(error);
+    app.post("/addbrand", async (req, res) => {
+      const brand = await req.body;
+
+      const result = await brandCollection.insertOne(brand);
+
+      res.send(result);
+    });
+  } finally {
   }
 };
 run().catch(console.dir);
+
+// Welcome route
+app.get("/", (req, res) => {
+  res.send(`<h1>Welcome to Foodie Backend!</h1>`);
+});
 
 // Creating the server
 app.listen(port, () => {
