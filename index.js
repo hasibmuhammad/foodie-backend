3eb69598b793983acae395cbdddf91e37109121b
hasibmuhammad <hasibmu99@gmail.com>
2023-12-06 01:47:43 +0600

    Delete cart api created

--- index.js
+++ index.js
@@ -108,6 +108,16 @@ const run = async () => {
       const result = await cartCollection.find().toArray();
       res.send(result);
     });
+
+    // delete cart
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: id };
+      const result = await cartCollection.deleteOne(query);
+
+      res.send(result);
+    });
   } finally {
   }
 };
